14571fff2673fb1f9c55c29741526b4b8b9ac5c6
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-04-01 14:42:25 +0200

    chore: add tanStack library

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import './App.css';
 import viteLogo from '/vite.svg';
 import { useState } from 'react';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import reactLogo from './assets/react.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const queryClient = new QueryClient();
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -30,7 +33,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      </QueryClientProvider>
   )
 }
 
