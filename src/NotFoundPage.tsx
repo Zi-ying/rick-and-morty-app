5f91c5f3e54bc0a091be6221e8b325873773eb82
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-04-02 17:05:49 +0200

    refactor: add errorMessage prop to NotFoundPage for better UX

--- src/NotFoundPage.tsx
+++ src/NotFoundPage.tsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
 
-const NotFoundPage = () => {
+interface NotFoundPageProps {
+  errorMessage?: string
+}
+
+const NotFoundPage = ({errorMessage}: NotFoundPageProps) => {
   return (
     <div className='grid gap-4 p-4'>
       <h1 className='text-gray-700'>Oops!</h1>
       <h5 className='text-gray-500'>We couldn't find the page you were looking for.</h5>
+      <h5 className='text-gray-500'>{errorMessage ? errorMessage : ''}</h5>
       <Link to="/">
         <button>Go home</button>
       </Link>
