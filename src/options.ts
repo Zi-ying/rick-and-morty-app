60624c0ca5b7d441b2bf2fe370dd5c12f9a4dd4e
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-04-03 11:33:32 +0200

    refactor: clean up

---
+++ src/options.ts
@@ -0,0 +1,12 @@
+export const genderOptions = [
+  { label: "Female", value: "female" },
+  { label: "Male", value: "male" },
+  { label: "Gender less", value: "genderless" },
+  { label: "Unknown", value: "unknown" },
+];
+
+export const statusOptions = [
+  { label: "Alive", value: "alive" },
+  { label: "Dead", value: "dead" },
+  { label: "Unknown", value: "unknown" },
+];
