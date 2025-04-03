f3bbbf79f9b5dd2fcfd9adba7ab3b996c1189531
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-04-03 15:04:07 +0200

    refactor: clean up

--- src/get-character-by-id.ts
+++ src/get-character-by-id.ts
@@ -1,6 +1,7 @@
 import { Character } from './types';
 
 export const getCharacterById = async (id: string): Promise<Character> => {
-  const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+  const url = `https://rickandmortyapi.com/api/character/${id}`
+  const response = await fetch(url);
   return response.json();
 }
