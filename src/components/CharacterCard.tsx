58f044e9c6bde40778d38140c9af88c9c436a16f
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-04-04 15:09:33 +0200

    refactor: update UI on character component

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,27 +1,31 @@
+import { cn } from '../lib/utils';
 import { Character } from '../types';
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 
 interface CharacterCardProps {
-  data: Character
+  data: Character;
 }
 
-const CharacterCard = ({data}: CharacterCardProps) => {
+const CharacterCard = ({ data }: CharacterCardProps) => {
   return (
-    <Card className='p-4'>
-    <img
-      src={data.image}
-      alt={`image of ${data.name} from Rick and Morty`}
-      className="rounded-lg"
-    />
-    <CardHeader>
-      <CardTitle>{data.name}</CardTitle>
-      <CardContent>{data.status}</CardContent>
-      <CardContent>{data.gender}</CardContent>
-      <CardContent>{data.species}</CardContent>
-      <CardContent>{data.type ? data.type : 'No data'}</CardContent>
-    </CardHeader>
-  </Card>
-  )
-}
+    <Card className="p-4">
+      <img
+        src={data.image}
+        alt={`image of ${data.name} from Rick and Morty`}
+        className="rounded-lg"
+      />
+      <CardHeader>
+        <CardTitle>{data.name}</CardTitle>
+        <CardContent className='flex gap-2 items-center justify-center'>
+          <div className={cn("rounded-full w-2 h-2", data.status === 'Alive' && 'bg-green-400', data.status === 'unknown' && 'bg-slate-500', data.status === 'Dead' && 'bg-red-500')} />
+          <p>{data.status}</p>
+        </CardContent>
+        <CardContent>{data.gender}</CardContent>
+        <CardContent>{data.species}</CardContent>
+        <CardContent>{data.type ? data.type : "No data"}</CardContent>
+      </CardHeader>
+    </Card>
+  );
+};
 
-export default CharacterCard
+export default CharacterCard;
