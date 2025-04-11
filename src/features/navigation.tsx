4eedc3e40abfed02d7aae2da6be830151814e9af
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-04-11 12:48:10 +0200

    refactor: udate navigation styling

--- src/features/navigation.tsx
+++ src/features/navigation.tsx
@@ -1,22 +1,27 @@
 import { ReactNode } from 'react';
+import { Link } from 'react-router-dom';
 
 interface NavigationProps {
   children: ReactNode;
 }
 
-const Navigation = ({children}: NavigationProps) => {
+const Navigation = ({ children }: NavigationProps) => {
   const image = "../../public/rick-and-morty.svg";
 
   return (
     <div className="text-white md:sticky left-0 top-0">
       <div className="grid grid-cols-2 p-2 bg-red-700">
-        <img src={image} alt="" className="h-20 bg-red-500" />
+        <img src={image} alt="" className="h-14 bg-red-500" />
         <div className="bg-red-500 flex justify-end gap-4 items-center">
-          <div className='bg-red-300'>Home</div>
-          <div className='bg-red-300'>Characters</div>
+          <Link to='/'>
+            <div className="bg-red-300">Home</div>
+          </Link>
+          <Link to='/character'>
+            <div className="bg-red-300">Characters</div>
+          </Link>
         </div>
       </div>
-      {children}
+      <div className="bg-red-700">{children}</div>
     </div>
   );
 };
