aebe152b919993f295e39507d3f2c0d61b0e507b
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-04-20 20:55:37 +0200

    feat: add search field to locations

--- src/features/locations/get-all-locations.ts
+++ src/features/locations/get-all-locations.ts
@@ -1,10 +1,21 @@
-import type { PaginationParams, Location } from '@/types/types';
+import type { PaginationParams, Location } from "@/types/types";
 
-const address = 'https://rickandmortyapi.com/api/location';
+const address = "https://rickandmortyapi.com/api/location";
 
-export const getAllLocations = async (): Promise<{ results: Location[]; info: PaginationParams }> => {
+interface GetAllLocationsParams {
+  filter: string;
+}
+
+export const getAllLocations = async ({
+  filter,
+}: GetAllLocationsParams): Promise<{
+  results: Location[];
+  info: PaginationParams;
+}> => {
   const url = new URL(address);
+
+  url.searchParams.append("name", filter);
 
   const response = await fetch(url);
   return response.json();
-}
+};
--- src/pages/LocationPage.tsx
+++ src/pages/LocationPage.tsx
@@ -1,22 +1,39 @@
+import { useState } from 'react';
+
 import { getAllLocations } from '@/features/locations/get-all-locations';
 import LocationsList from '@/features/locations/locationsList';
 import Navigation from '@/features/navigation';
+import SearchField from '@/features/searchFields/SearchField';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 
+import { useDebounce } from '../features/searchFields/use-debounce';
+
 const LocationPage = () => {
+  const [search, setSearch] = useState<string>("");
+
+  const timeout = 500;
+  const debouncedSearch = useDebounce(search, timeout);
+
   const { data } = useQuery({
     queryKey: ["locationsData"],
-    queryFn: getAllLocations,
+    queryFn: () => getAllLocations({ filter: debouncedSearch }),
     placeholderData: keepPreviousData,
   });
 
   if (!data?.results) {
-    return <>No Data found</>
+    return <>No Data found</>;
   }
 
   return (
     <>
-      <Navigation />
+      <Navigation>
+        <SearchField
+          placeholder="Search for a location"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="justify-self-center max-w-96"
+        />
+      </Navigation>
       <div className="bg-red-400 grid p-2 gap-2">
         <div>LOCATIONS</div>
         <div className="bg-red-300 grid md:grid-cols-2 gap-2">
