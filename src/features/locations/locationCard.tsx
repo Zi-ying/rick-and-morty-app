5328b262ca8b466f7ec1dc433b2ffd1e25ea8547
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-04-20 19:52:53 +0200

    feat: add locationcard component

---
+++ src/features/locations/locationCard.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+
+const LocationCard = () => {
+  return (
+    <>
+      <Card>
+        <CardHeader>Location</CardHeader>
+        <CardContent>Location</CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default LocationCard;
