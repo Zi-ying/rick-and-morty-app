0cc80aca7afb947cc1945f082145257d41f1831c
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-04-20 19:53:17 +0200

    feat: add location page component

--- src/pages/LocationPage.tsx
+++ src/pages/LocationPage.tsx
@@ -1,10 +1,16 @@
-import Navigation from '../features/navigation';
+import LocationsList from '@/features/locations/locationsList';
+import Navigation from '@/features/navigation';
 
 const LocationPage = () => {
   return (
     <>
       <Navigation />
-      <div>LOCATIONS</div>
+      <div className="bg-red-400 grid p-2 gap-2">
+        <div>LOCATIONS</div>
+        <div className='bg-red-300 grid md:grid-cols-2 gap-2'>
+          <LocationsList />
+        </div>
+      </div>
     </>
   );
 };
