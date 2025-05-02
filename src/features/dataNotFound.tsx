64822e9008a07be9d281d22c3e4e209620a18967
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-02 10:37:52 +0200

    refactor: improve no results found UI

--- src/features/dataNotFound.tsx
+++ src/features/dataNotFound.tsx
@@ -1,13 +1,13 @@
 const DataNotFound = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="grid gap-4 justify-items-center p-4 backdrop-blur-xs rounded-xl m-auto">
+      <div className="grid gap-4 justify-items-center p-4 backdrop-blur-xs rounded-xl m-auto backdrop-brightness-80">
         <h1 className="text-pickle-500 text-9xl">Oops!</h1>
         <h5 className="text-white text-5xl">No results found</h5>
         <h5 className="text-white text-4xl">
           We could not find what you searched for.
         </h5>
-        <h5 className="text-white text-3xl">Try searching again.</h5>
+        <h5 className="text-white text-3xl contrast-200">Try searching again.</h5>
       </div>
     </div>
   );
