996147418b4eb396556f6040fd86a7979660fd05
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-06 15:54:28 +0200

    refactor: improve spinner UI

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -6,7 +6,7 @@ const LoadingSpinner = ({ message = 'Loading...' }: LoadingSpinnerProps) => {
   return (
     <div className="grid gap-4 items-center justify-center p-4">
       <div
-      className="inline-block self-justify-center w-10 h-10 border-4 rounded-full border-pickle-500 animate-spin"
+      className="inline-block self-justify-center size-10 border-4 rounded-full border-pickle-500 animate-spin"
       />
       <p className="text-pickle-500">{message}</p>
     </div>
