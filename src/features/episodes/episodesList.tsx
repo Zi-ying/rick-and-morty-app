c77eb06253b32a5ea38d1864d4d8142d15402797
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-08 10:05:30 +0200

    chore: cleanup

--- src/features/episodes/episodesList.tsx
+++ src/features/episodes/episodesList.tsx
@@ -2,12 +2,11 @@ import { Link } from 'react-router-dom';
 
 import Spinner from '@/components/ui/spinner';
 
-import ResultsNotFound from '../resultsNotFound';
 import EpisodeCard from './episodeCard';
 
-import type { Episode, PaginationParams } from "@/types/types";
+import type { Episode } from "@/types/types";
 interface EpisodesListProps {
-  data: { results: Episode[]; info: PaginationParams } | undefined;
+  data: Episode[];
   isPending: boolean;
 }
 
@@ -15,17 +14,14 @@ const EpisodesList = ({
   data,
   isPending,
 }: EpisodesListProps) => {
+
   if (isPending) {
     return <Spinner />;
   }
 
-  if (!data?.info && !data?.results) {
-    return <ResultsNotFound />;
-  }
-
   return (
     <>
-      {data.results.map((item) => {
+      {data.map((item) => {
         return (
           <Link key={item.id} to={item.id.toString()}>
             <EpisodeCard data={item} />
