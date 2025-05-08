660c55af560a2a033fe073935f2d90c4d406ede5
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-08 10:13:29 +0200

    feat: fetch data from multipe episodes

---
+++ src/features/episodes/get-multiple-episodes.ts
@@ -0,0 +1,12 @@
+import type { Episode } from "@/types/types";
+
+export const getMultipleEpisodes = async (
+  ids: string
+): Promise<Episode[]> => {
+  const address = `https://rickandmortyapi.com/api/episode/${ids}`;
+  const url = new URL(address);
+
+  const response = await fetch(url);
+
+  return response.json();
+};
