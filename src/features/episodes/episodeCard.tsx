280fd2cfef7a167fbc79b4ec9db811f5262b27da
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-08 09:59:30 +0200

    refactor: improve UI

--- src/features/episodes/episodeCard.tsx
+++ src/features/episodes/episodeCard.tsx
@@ -8,10 +8,10 @@ interface EpisodeCardProps {
 
 const EpisodeCard = ({ data }: EpisodeCardProps) => {
   return (
-    <Card>
-      <CardHeader>{data.name}</CardHeader>
-      <CardContent>{data.air_date}</CardContent>
-      <CardContent>{data.episode}</CardContent>
+    <Card className='rounded-xl hover:shadow-xl shadow-pickle-400/50 backdrop-blur-md py-2'>
+      <CardHeader className='text-pickle-400'>{data.name}</CardHeader>
+      <CardContent className='text-slate-300'>{data.air_date}</CardContent>
+      <CardContent className='text-slate-300'>{data.episode}</CardContent>
     </Card>
   );
 };
