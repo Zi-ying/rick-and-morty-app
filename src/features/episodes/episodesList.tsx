5048478a0ea54d4ebfb1f5d443d7683737ba164c
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-08 09:59:59 +0200

    chore: cleanup

--- src/features/episodes/episodesList.tsx
+++ src/features/episodes/episodesList.tsx
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 
 import Spinner from '@/components/ui/spinner';
 
-import PaginationList from '../pagination/paginationList';
 import ResultsNotFound from '../resultsNotFound';
 import EpisodeCard from './episodeCard';
 
@@ -10,15 +9,11 @@ import type { Episode, PaginationParams } from "@/types/types";
 interface EpisodesListProps {
   data: { results: Episode[]; info: PaginationParams } | undefined;
   isPending: boolean;
-  currentPage: number;
-  setPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const EpisodesList = ({
   data,
   isPending,
-  currentPage,
-  setPage,
 }: EpisodesListProps) => {
   if (isPending) {
     return <Spinner />;
@@ -28,11 +23,8 @@ const EpisodesList = ({
     return <ResultsNotFound />;
   }
 
-  const maxPage = data.info.pages;
-
-
   return (
-    <div className="bg-red-300 grid md:grid-cols-2 gap-2">
+    <>
       {data.results.map((item) => {
         return (
           <Link key={item.id} to={item.id.toString()}>
@@ -40,12 +32,7 @@ const EpisodesList = ({
           </Link>
         );
       })}
-      <PaginationList
-        currentPage={currentPage}
-        maxPage={maxPage}
-        setCurrentPage={setPage}
-      />
-    </div>
+    </>
   );
 };
 
