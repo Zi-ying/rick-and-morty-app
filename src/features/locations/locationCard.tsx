29ee433e5d560a58287d4d452acdf436c969df31
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-09 09:55:38 +0200

    refactor: improve location card UI

--- src/features/locations/locationCard.tsx
+++ src/features/locations/locationCard.tsx
@@ -8,10 +8,10 @@ interface LocationCardProps {
 
 const LocationCard = ({ data }: LocationCardProps) => {
   return (
-    <Card>
-      <CardHeader>{data.name}</CardHeader>
-      <CardContent>{data.type}</CardContent>
-      <CardContent>{data.dimension}</CardContent>
+    <Card className="rounded-xl hover:shadow-xl shadow-pickle-400/50 backdrop-blur-md py-2">
+      <CardHeader className="text-pickle-400">{data.name}</CardHeader>
+      <CardContent className="text-slate-300">{data.type}</CardContent>
+      <CardContent className="text-slate-300">{data.dimension}</CardContent>
     </Card>
   );
 };
