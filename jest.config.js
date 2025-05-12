02591499a052436b2dfe031b13d582f37366b242
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-12 15:15:40 +0200

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+      tsconfig: 'tsconfig.jest.json'
+    }],
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+};
