2877f1d472a7cefaf6113886c3f845ef28051421
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-12 11:30:02 +0200

    chore: cleanup

--- src/features/episodes/episodesList.tsx
+++ src/features/episodes/episodesList.tsx
@@ -1,34 +1,126 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Spinner from '@/components/ui/spinner';
+import { cn } from '@/lib/utils';
+import { addFilter, allFilters, removeOneFilter, resetFilters } from '@/store/filters-slice';
+import { useAppDispatch } from '@/store/redux-hooks';
+import { useDebounce } from '@/utils/use-debounce';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 
+import ExpansionButton from '../expansionButton';
+import PaginationList from '../pagination/paginationList';
+import ResultsNotFound from '../resultsNotFound';
+import FilterBadges from '../searchFields/filterBadges';
+import SearchInput from '../searchFields/searchInput';
+import SelectInput from '../searchFields/selectInput';
 import EpisodeCard from './episodeCard';
+import { getAllEpisodes } from './get-all-episodes';
+import { episodeOptions } from './options';
 
-import type { Episode } from "@/types/types";
-interface EpisodesListProps {
-  data: Episode[];
-  isPending: boolean;
-}
+import type { EpisodeFilterParams, Filters } from "@/types/types";
+const EpisodesList = () => {
+  const filters = useSelector(allFilters);
+  const [page, setPage] = useState<number>(1);
+  const [search, setSearch] = useState<string>(filters.episodeName);
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
 
-const EpisodesList = ({
-  data,
-  isPending,
-}: EpisodesListProps) => {
+  const timeout = 500;
+  const debouncedSearch = useDebounce(search, timeout);
 
-  if (isPending) {
-    return <Spinner />;
-  }
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    setPage(1);
+  };
+
+  useEffect(() => {
+    dispatch(addFilter({ key: "episodeName", value: debouncedSearch }));
+  }, [dispatch, search, debouncedSearch]);
+
+  const onResetClick = () => {
+    dispatch(resetFilters());
+    setPage(1);
+  };
+
+  const filterArgs: EpisodeFilterParams = {
+    name: filters.episodeName,
+    episode: filters.episode,
+  };
+
+  const { data, isPending } = useQuery({
+    queryKey: ["episodesData", filterArgs, page],
+    queryFn: () =>
+      getAllEpisodes({ filters: filterArgs, page: page.toString() }),
+    placeholderData: keepPreviousData,
+  });
+
+  const handleClear = (filter: keyof Filters) => {
+    dispatch(removeOneFilter(filter));
+    setPage(1);
+  };
+
+  const setFilter = (value: string) => {
+    dispatch(addFilter({ key: "episode", value }));
+    setPage(1);
+  };
 
   return (
-    <>
-      {data.map((item) => {
-        return (
-          <Link key={item.id} to={item.id.toString()}>
-            <EpisodeCard data={item} />
-          </Link>
-        );
-      })}
-    </>
+    <div className="space-y-2 pt-2">
+      <div className="grid grid-cols-4 gap-2">
+        <SearchInput
+          placeholder="Search for an episode"
+          value={search}
+          className="p-4 text-white col-start-2 col-end-4"
+          onChange={onChange}
+        />
+        <ExpansionButton
+          expanded={isExpanded}
+          onClick={() => setIsExpanded(!isExpanded)}
+        />
+      </div>
+      <div
+        className={cn(
+          "justify-center text-white w-full",
+          isExpanded ? "inline-flex" : "hidden"
+        )}
+      >
+        <SelectInput
+          placeholder="Episodes"
+          value={filters.episode}
+          data={episodeOptions}
+          onChange={setFilter}
+        />
+      </div>
+      <FilterBadges
+        filters={filters}
+        onClearOne={handleClear}
+        onClearAll={onResetClick}
+        className="flex flex-wrap gap-2 justify-center items-center"
+      />
+      {data?.info && data?.results && (
+        <PaginationList
+          currentPage={page}
+          maxPage={data?.info.pages ?? 0}
+          setCurrentPage={setPage}
+        />
+      )}
+      {isPending && <Spinner />}
+      {!data?.info && !data?.results ? (
+        <ResultsNotFound />
+      ) : (
+        <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-4 p-2">
+          {data.results.map((item) => {
+            return (
+              <Link key={item.id} to={item.id.toString()}>
+                <EpisodeCard data={item} />
+              </Link>
+            );
+          })}
+        </div>
+      )}
+    </div>
   );
 };
 
