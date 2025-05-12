14768026695cd09c7adf31b21d3d0c9b53293401
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-12 15:45:19 +0200

    fix: remove navigation on favorite toggle

--- src/features/characters/charactersList.tsx
+++ src/features/characters/charactersList.tsx
@@ -64,7 +64,7 @@ const CharactersList = () => {
       <CharactersFavorites
         isFavoritePage={isFavoritePage}
         setIsFavoritePage={() => setIsFavoritePage(!isFavoritePage)}
-        onToggle={() => onToggle}
+        onToggle={onToggle}
       />
     );
   }
