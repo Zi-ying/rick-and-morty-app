7f74d3293355934c818345229d5c8ce8cb80b96e
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-13 11:21:57 +0200

    refactor: update toggle heart UI and UX

--- src/components/heartToggle.tsx
+++ src/components/heartToggle.tsx
@@ -6,16 +6,16 @@ import { cn } from '@/lib/utils';
 interface HeartToggleProps {
   isToggled: boolean;
   variant?: "default" | "outline";
-  onToggle: (e: React.ToggleEvent<HTMLDivElement>) => void;
+  onToggle: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
 const heartToggleVariants = cva(
-  "[&>svg]:stroke-pink-600 [&>svg]:size-6 hover:[&>svg]:fill-pink-800 hover:[&>svg]:stroke-pink-800 cursor-pointer inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium",
+  "[&>svg]:stroke-pink-600 hover:[&>svg]:fill-pink-800 hover:[&>svg]:stroke-pink-800 cursor-pointer inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium",
   {
     variants: {
       variant: {
-        default: "[&>svg]:stroke-1 [&>svg]:size-6",
-        outline: "border border-white [&>svg]:stroke-2 h-9 px-2",
+        default: "[&>svg]:stroke-1.5 [&>svg]:size-5",
+        outline: "border border-white [&>svg]:stroke-2 [&>svg]:size-6 h-9 px-2",
       },
     },
     defaultVariants: {
@@ -28,17 +28,15 @@ const HeartToggle = ({
   isToggled,
   variant = "default",
   onToggle,
-}: HeartToggleProps &
-  React.ComponentProps<"div"> &
-  VariantProps<typeof heartToggleVariants>) => {
+}: HeartToggleProps & VariantProps<typeof heartToggleVariants>) => {
   return (
     <div
       aria-label="Toggle heart"
-      onToggle={onToggle}
+      onClick={onToggle}
       className={cn(
         heartToggleVariants({ variant }),
         isToggled &&
-          "[&>svg]:fill-pink-600 transition delay-150 [&>svg]:stroke-4"
+          "[&>svg]:fill-pink-600 transition delay-150 [&>svg]:stroke-3"
       )}
     >
       <Heart />
