90eb79c41978ac18d20a32d809af6be0de96febe
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-13 09:43:08 +0200

    feat: add home component

---
+++ src/features/home.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+import { Button } from '@/components/ui/button';
+
+const Home = () => {
+  return (
+    <div className="h-[calc(100vh-56px)] flex items-center justify-center">
+      <Link to="/character">
+        <Button>Get Started!</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
