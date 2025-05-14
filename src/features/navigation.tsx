ffa701bf8391afba1185820ece88fda6c060ede5
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-14 10:24:30 +0200

    refactor: improve code

--- src/features/navigation.tsx
+++ src/features/navigation.tsx
@@ -6,49 +6,46 @@ import { cn } from '@/lib/utils';
 const Navigation = () => {
   const location = useLocation();
 
+  const locationOptions = [
+    {
+      value: "/",
+      label: "Home",
+    },
+    {
+      value: "/character",
+      label: "Characters",
+    },
+    {
+      value: "/location",
+      label: "Locations",
+    },
+    {
+      value: "/episode",
+      label: "Episodes",
+    },
+  ];
+
   return (
     <div className="text-white sm:sticky left-0 top-0 shadow-2xl z-10 bg-home flex px-2">
-        <Link to='/' className='flex-none'>
-          <img src={svg} alt="Rick and Morty" className="h-14" />
-        </Link>
-        <div className="flex justify-end gap-4 items-center grow">
-          <Link
-            to="/"
-            className={cn(
-              "hover:text-pickle-500 hover:underline underline-offset-8 py-4",
-              location.pathname === "/" ? "underline" : ""
-            )}
-          >
-            Home
-          </Link>
-          <Link
-            to="/character"
-            className={cn(
-              "hover:text-pickle-500 hover:underline underline-offset-8 py-4",
-              location.pathname === "/character" ? "underline" : ""
-            )}
-          >
-            Characters
-          </Link>
-          <Link
-            to="/location"
-            className={cn(
-              "hover:text-pickle-500 hover:underline underline-offset-8 py-4",
-              location.pathname === "/location" ? "underline" : ""
-            )}
-          >
-            Locations
-          </Link>
-          <Link
-            to="/episode"
-            className={cn(
-              "hover:text-pickle-500 hover:underline underline-offset-8 py-4",
-              location.pathname === "/episode" ? "underline" : ""
-            )}
-          >
-            Episodes
-          </Link>
-        </div>
+      <Link to="/" className="flex-none">
+        <img src={svg} alt="Rick and Morty" className="h-14" />
+      </Link>
+      <div className="flex justify-end gap-4 items-center grow">
+        {locationOptions.map((option, index) => {
+          return (
+            <Link
+              key={index}
+              to={option.value}
+              className={cn(
+                "hover:text-pickle-500 hover:underline underline-offset-8 py-4",
+                location.pathname === option.value && "underline"
+              )}
+            >
+              <p>{option.label}</p>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
