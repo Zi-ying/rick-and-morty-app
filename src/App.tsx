0348eb112e7e08b9c7891074674e28edd0148f8d
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-28 16:53:14 +0200

    chore: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,13 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/character" element={<CharactersPage />} />
-        <Route path="/character/:characterId" element={<CharacterPage />} />
-        <Route path="/favorite" element={<FavoritePage />} />
-        <Route path="/location" element={<LocationsPage />} />
-        <Route path="/location/:locationId" element={<LocationPage />} />
-        <Route path="/episode" element={<EpisodesPage />} />
-        <Route path="/episode/:episodeId" element={<EpisodePage />} />
+        <Route path="character" element={<CharactersPage />} />
+        <Route path="character/:characterId" element={<CharacterPage />} />
+        <Route path="favorite" element={<FavoritePage />} />
+        <Route path="location" element={<LocationsPage />} />
+        <Route path="location/:locationId" element={<LocationPage />} />
+        <Route path="episode" element={<EpisodesPage />} />
+        <Route path="episode/:episodeId" element={<EpisodePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
