1b7a43405f8c2a3f7fb5e5f6568f9d8a52bfd67d
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-05-29 09:34:36 +0200

    refactor: cleanup

--- src/features/characters/charactersList.tsx
+++ src/features/characters/charactersList.tsx
@@ -1,9 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
-import Spinner from '@/components/ui/spinner';
-import SmallCharacterCard from '@/features/character/smallCharacterCard';
-import { addFavorite, allFavorites, removeFavorite } from '@/store/favorites-slice';
 import { addFilter, allFilters, removeOneFilter, resetFilters } from '@/store/filters-slice';
 import { useAppDispatch, useAppSelector } from '@/store/redux-hooks';
 import { Filters } from '@/types/filters';
@@ -12,17 +8,13 @@ import { keepPreviousData, useQuery } from '@tanstack/react-query';
 
 import { SearchInput } from '../inputs';
 import Navigation from '../navigation';
-import PaginationList from '../pagination/paginationList';
-import ResultNotFound from '../resultNotFound';
-import CharacterCard from './characterCard';
 import CharactersFilterBar from './charactersFilterBar';
+import CharactersListItems from './charactersListItems';
 import { getAllCharacters } from './get-all-characters';
 
-import type { Character, CharacterFilters } from "./types";
+import type { CharacterFilters } from "./types";
 const CharactersList = () => {
-  const keys = useAppSelector(allFavorites);
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
   const filters: Filters = useAppSelector(allFilters);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [name, setName] = useState<string>(filters.characterName);
@@ -68,33 +60,7 @@ const CharactersList = () => {
     placeholderData: keepPreviousData,
   });
 
-  const getFavorite = (item: Character) => {
-    return keys.includes(item.id.toString());
-  };
-
-  const onToggle = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    item: Character
-  ) => {
-    e.stopPropagation();
-    const isFavorite = getFavorite(item);
-
-    if (!isFavorite) {
-      dispatch(addFavorite({ key: item.id.toString(), value: item.name }));
-    } else {
-      dispatch(removeFavorite(item.id.toString()));
-    }
-  };
-
   if (error) return "An error has occurred: " + error.message;
-
-  if (isPending) {
-    return (
-      <div className="h-[calc(100vh-56px)] flex items-center justify-center">
-        <Spinner size="lg" />
-      </div>
-    );
-  }
 
   return (
     <>
@@ -112,55 +78,7 @@ const CharactersList = () => {
         onClear={handleClear}
         onReset={onResetClick}
       />
-      {data?.info && data?.results ? (
-        <>
-          <div className="w-full h-full hidden md:grid md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-6 overflow-y-auto p-4">
-            {data.results.map((item) => {
-              const isFavorite = getFavorite(item);
-
-              return (
-                <CharacterCard
-                  key={item.id}
-                  data={item}
-                  isPending={isPending}
-                  isFavorite={isFavorite}
-                  onClick={() => {
-                    navigate(`/character/${item.id}`);
-                  }}
-                  onToggle={(e) => onToggle(e, item)}
-                />
-              );
-            })}
-          </div>
-          <div className="w-full grid sm:grid-cols-2 p-6 gap-3 md:hidden">
-            {data.results.map((item) => {
-              const isFavorite = getFavorite(item);
-
-              return (
-                <SmallCharacterCard
-                  key={item.id}
-                  data={item}
-                  isPending={isPending}
-                  isFavorite={isFavorite}
-                  hasToggle
-                  onClick={() => {
-                    navigate(`/character/${item.id}`);
-                  }}
-                  onToggle={(e) => onToggle(e, item)}
-                />
-              );
-            })}
-          </div>
-          <PaginationList
-            currentPage={currentPage}
-            maxPage={data.info.pages}
-            setCurrentPage={setCurrentPage}
-            className="p-4 mt-auto"
-          />
-        </>
-      ) : (
-        <ResultNotFound />
-      )}
+      <CharactersListItems data={data} isPending={isPending} currentPage={currentPage} setCurrentPage={setCurrentPage} />
     </>
   );
 };
