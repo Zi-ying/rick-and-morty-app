5ef24ead584431b1a956d2653f2eeed2371d1d10
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-06-05 20:25:18 +0200

    feat: add location options data

---
+++ src/features/navigation/options.ts
@@ -0,0 +1,22 @@
+export const locationOptions = [
+  {
+    value: "/",
+    label: "Home",
+  },
+  {
+    value: "/character",
+    label: "Characters",
+  },
+  {
+    value: "/location",
+    label: "Locations",
+  },
+  {
+    value: "/episode",
+    label: "Episodes",
+  },
+  {
+    value: "/favorite",
+    label: "Favorites",
+  },
+];
