c90c7dda6147e98c44718aa8c8c095e4c39a0591
Sophie Bel <sophie.belmarrocco@gmail.com>
2025-06-09 19:25:21 +0200

    refactor: improve code maintainability for status circle component

--- src/features/characters/status-circle.tsx
+++ src/features/characters/status-circle.tsx
@@ -2,9 +2,13 @@ import { Circle } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 
-import type { Character } from './types';
+import type { Character } from "./types";
 
-const StatusCircle = (({ status }: { status: Character['status'] }) => (
+interface StatusCircleProps {
+  status: Character["status"];
+}
+
+const StatusCircle = ({ status }: StatusCircleProps) => (
   <Circle
     className={cn(
       "h-2.5 stroke-2.5",
@@ -14,6 +18,6 @@ const StatusCircle = (({ status }: { status: Character['status'] }) => (
     )}
     aria-label={`Status: ${status}`}
   />
-));
+);
 
 export default StatusCircle;
